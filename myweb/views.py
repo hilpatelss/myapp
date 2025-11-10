abf5f23c5f5775708381c734418f12d4c505db0f
hil patel <113950702+hilpatelss@users.noreply.github.com>
2025-11-10 07:07:20 +0530

    Refactor home view to streamline queries

--- myweb/views.py
+++ myweb/views.py
@@ -8,7 +8,7 @@ from myweb.models import *
 def home(request):
     try:
         query1 = Profile.objects.get( p_no = 1)
-        query4 = Fav.objects.get( f_no = 1)
+       
         query2_1 = Logo.objects.get( l_no = 1)
         query2_2 = Logo.objects.get( l_no = 2)
         query2_3 = Logo.objects.get( l_no = 3)
@@ -19,10 +19,11 @@ def home(request):
         query2_8 = Logo.objects.get( l_no = 8)
         query3 = Data.objects.filter().order_by('d_no')
 
-        context = {'page': 'Hil Patel' , 'q4':query4 ,'q1':query1 , 'q2_1':query2_1 , 'q2_2':query2_2, 'q2_3':query2_3 , 'q2_4':query2_4 , 'q2_5':query2_5, 'q2_6':query2_6, 'q2_7':query2_7, 'q2_8':query2_8, 'q3':query3 }
+        context = {'page': 'Hil Patel' ,'q1':query1 , 'q2_1':query2_1 , 'q2_2':query2_2, 'q2_3':query2_3 , 'q2_4':query2_4 , 'q2_5':query2_5, 'q2_6':query2_6, 'q2_7':query2_7, 'q2_8':query2_8, 'q3':query3 }
 
         return render(request , "index.html", context)
     except:
         context = {'page': 'Hil Patel'}
 
         return render(request , "index.html", context)
+
